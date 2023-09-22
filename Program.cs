6cf4177e65cf64bc4f3c41c24ab7abf9380ed31b
Edvin Amborg <edvin.amborg@gmail.com>
2023-09-22 11:16:13 +0200

    Uppgift 4620 steg 5

--- Program.cs
+++ Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -32,6 +34,10 @@ namespace Introduktion_C_
             //lösning.Uppgift_9A();//
             //lösning.Uppgift_9B();//
             //lösning.Uppgift_10A();//
+            //lösning.Uppgift4620_steg2();
+            //lösning_Uppgift4620_steg3();
+            //lösning.Uppgift4620_steg4();
+            lösning.Uppgift4620_steg5();
 
         }
     }
@@ -42,16 +48,17 @@ namespace Introduktion_C_
         {
         }
 
-        /*
+
+
         public void Uppgift_1A()
         {
             Console.WriteLine("Vad heter du?");
             string namn = Console.ReadLine();
             Console.WriteLine("hej " + namn + ", varmt välkommen!");
         }
-        */
 
-        /*
+
+
         public void Uppgift_1B()
         {
             var bredd = 9.6;
@@ -61,10 +68,10 @@ namespace Introduktion_C_
             Console.WriteLine("Höjd = " + höjd);
             Console.WriteLine("Area = " + area);
         }
-        */
 
 
-        /*
+
+
         public void Uppgift_1C()
         {
             Console.WriteLine("Skriv triangelns bredd: ");
@@ -75,10 +82,12 @@ namespace Introduktion_C_
             double Höjd = Convert.ToDouble(höjd);
             var area = (Bredd * Höjd) / 2;
             Console.WriteLine("Area = " + area);
-        */
+        }
 
 
-        /*
+
+
+
 
         public void Uppgift_1D()
         {
@@ -92,11 +101,11 @@ namespace Introduktion_C_
             Console.WriteLine("Summan av talen är: " + summa);
 
         }
-        */
 
 
 
-        /*
+
+
 
         public void Uppgift_2A()
         {
@@ -112,11 +121,11 @@ namespace Introduktion_C_
                 Console.WriteLine("Tyvärr, du gissade fel");
             }
         }
-        */
 
 
 
-        /*
+
+
         public void Uppgift_2B()
         {
             Random randomGenerator = new Random();
@@ -135,9 +144,9 @@ namespace Introduktion_C_
 
         }
 
-        */
 
-        /*
+
+
         public void Uppgift_3A()
         {
             int nummer = 1;
@@ -147,9 +156,9 @@ namespace Introduktion_C_
                 nummer++;
             }
         }
-        */
 
-        /*
+
+
         public void Uppgift_3B()
         {
             int nummer = 5;
@@ -159,10 +168,10 @@ namespace Introduktion_C_
                 nummer = nummer + 3;
             }
         }
-        */
 
 
-        /*
+
+
         public void Uppgift_3C()
         {
             int nummer = 10;
@@ -172,10 +181,10 @@ namespace Introduktion_C_
                 nummer--;
             }
         }
-        */
 
 
-        /*
+
+
         public void Uppgift_4A()
         {
             for (int räknare = 1; räknare <= 5; räknare = räknare + 1)
@@ -183,10 +192,10 @@ namespace Introduktion_C_
                 Console.WriteLine(räknare);
             }
         }
-        */
 
 
-        /*
+
+
         public void Uppgift_4B()
         {
             for (int i = 5; i <= 20; i = i + 3)
@@ -194,10 +203,10 @@ namespace Introduktion_C_
                 Console.WriteLine(i);
             }
         }
-        */
 
 
-        /*
+
+
         public void Uppgift_4C()
         {
             for (int i = 10; i >= 0; i--)
@@ -205,10 +214,10 @@ namespace Introduktion_C_
                 Console.WriteLine(i);
             }
         }
-        */
 
 
-        /*
+
+
         public void Uppgift_5()
         {
             Console.WriteLine("Gissa på ett tal: ");
@@ -223,10 +232,11 @@ namespace Introduktion_C_
                 hej = Convert.ToInt32(t);
             }
             Console.WriteLine("Grattis");
-        */
+        }
 
 
-        /*
+
+
         public void Uppgift_6()
         {
             Random randomGenerator = new Random();
@@ -251,11 +261,11 @@ namespace Introduktion_C_
             }
 
         }
-        */
 
 
 
-        /*
+
+
         public void Uppgift_7()
         {
             int[] tal = new int[6];
@@ -274,11 +284,12 @@ namespace Introduktion_C_
                 int z = y + 1;
                 Console.WriteLine(z + ", ");
             }
-        */
+        }
 
 
 
-        /*
+
+
         public void Uppgift_9A()
         {
             Console.WriteLine("Vad är ditt namn?");
@@ -292,12 +303,12 @@ namespace Introduktion_C_
         {
             string hej = ("Välkommen Agent " + namn + ". Ditt uppdrag är...");
             return hej;
-            
+
         }
-        */
 
 
-        /*
+
+
         public void Uppgift_9B()
         {
             Console.WriteLine("Skriv in ett tal: ");
@@ -308,7 +319,7 @@ namespace Introduktion_C_
             var Taltvå = Console.ReadLine();
             int talett = Convert.ToInt32(Taltvå);
 
-            
+
 
             Console.WriteLine("Deras summa är: " + Addera(talett, taltvå));
         }
@@ -318,11 +329,11 @@ namespace Introduktion_C_
             int Summan = ett + två;
             return Summan;
         }
-        */
 
 
-        /*
-        public void Uppgift_9B()
+
+
+        public void Uppgift_9B2()
         {
             Console.WriteLine("Skriv in ett tal: ");
             var Talett = Console.ReadLine();
@@ -350,16 +361,16 @@ namespace Introduktion_C_
             Console.WriteLine("Deras summa är: " + summa3);
         }
 
-        private int Addera(int ett, int två)
+        private int Addera_2(int ett, int två)
         {
             int Summan = ett + två;
             return Summan;
         }
-        */
 
 
 
-        /*
+
+
         public void Uppgift_10A()
         {
             string[] inköpslista = new string[10];
@@ -369,13 +380,13 @@ namespace Introduktion_C_
 
             //kan lägga till fler varor tills man svarar nej
             while (svar == "j")
-            { 
+            {
                 Console.WriteLine("Vad vill du lägga till? ");
 
                 //lägger till varan i listan
                 string vara = Console.ReadLine();
                 inköpslista[i] = vara;
-                
+
                 Console.WriteLine("Vill du lägga till något mer i din inköpslista? j/n ");
                 svar = Console.ReadLine();
 
@@ -393,18 +404,18 @@ namespace Introduktion_C_
                     Console.WriteLine(sak);
                 }
 
-                
+
             }
-            
+
         }
-        */
 
 
 
 
 
-        /*
-        public void Uppgift_7()
+
+
+        public void Uppgift_7_2()
         {
             int[] tal = new int[6];
 
@@ -422,7 +433,178 @@ namespace Introduktion_C_
                 int z = y + 1;
                 Console.WriteLine(z + ", ");
             }
+        }
+
+
+
+
+
+
+        public void Uppgift4620_steg2()
+        {
+            string[] frågor = new string[3];
+            frågor[0] = "Vad heter Norges huvudstad? ";
+            frågor[1] = "Vad heter Danmarks huvustad? ";
+            frågor[2] = "Vad heter Finlands huvustad";
+
+
+            string[] svar = new string[3];
+            svar[0] = "oslo";
+            svar[1] = "köpenhamn";
+            svar[2] = "helsingfors";
+
+
+            int i = 0;
+            foreach (string fråga in frågor)
+            {
+
+                //Fråga och läs in svaret
+                Console.WriteLine(fråga);
+                string svaret = Console.ReadLine();
+
+                //kolla om svaret är rätt
+                if (svaret == svar[i])
+                {
+                    Console.WriteLine("Rätt!");
+                }
+                else
+                {
+                    Console.WriteLine("Fel");
+                }
+
+                //Ändra rätta svaret till nästa fråga
+                i++;
+            }
+        }
+
+
+
+
+        public void lösning_Uppgift4620_steg3()
+        {
+            List<string> frågor;
+            frågor = new List<string>();
+            frågor.Add("Vad heter Norges huvudstad? ");
+            frågor.Add("Vad heter Danmarks huvudstad? ");
+            frågor.Add("Vad heter Finlands huvudstad? ");
+
+            List<string> rättsvar;
+            rättsvar = new List<string>();
+            frågor.Add("oslo");
+            frågor.Add("köpenhamn");
+            frågor.Add("helsingfors");
+
+            int i = 0;
+            foreach (string fråga in frågor)
+
+                //Fråga och läs in svaret
+                Console.WriteLine(fråga);
+            string svar = Console.ReadLine();
+
+            //kolla om svaret stämmer med det rätta svaret
+            if (svar == rättsvar[i])
+            {
+                Console.WriteLine("Rätt!");
+            }
+            else
+            {
+                Console.WriteLine("Fel");
+            }
+            //ändra det rätta svaret inför nästa fråga
+            i++;
+        }
+
+
+        /*
+        class QuestionCard
+        {
+            public String fråga;
+            public String svar;           
+        }
+        public void Uppgift4620_steg4()
+        {
+            
+            List<QuestionCard> frågekort = new List<QuestionCard>();
+
+
+            QuestionCard card1 = new QuestionCard();
+            card1.fråga = "Vad heter Norges Huvudstad";
+            card1.svar = "oslo";
+            frågekort.Add(card1);
+
+            QuestionCard card2 = new QuestionCard();
+            card2.fråga = "Vad heter Finlands Huvudstad";
+            card2.svar = "helsingfors";
+            frågekort.Add(card2);
+
+
+            QuestionCard card3 = new QuestionCard();
+            card3.fråga = "Vad heter Sveriges Huvudstad";
+            card3.svar = "stockholm";
+            frågekort.Add(card3);
+
+            foreach (QuestionCard f in frågekort)
+            {
+                Console.WriteLine(f.fråga);
+                string s = Console.ReadLine().ToLower();
+                if (s == f.svar)
+                {
+                    Console.WriteLine("Rätt!");
+                }
+                else
+                {
+                    Console.WriteLine("Fel");
+                }
+            }
+        }
         */
+
+
+
+        public void Uppgift4620_steg5()
+        {
+            List<QuestionCard> frågekort = new List<QuestionCard>();
+
+            QuestionCard card1; 
+            card1 = new QuestionCard("Vad heter Norges huvudtsad?", "oslo");
+            frågekort.Add(card1);
+
+            QuestionCard card2;
+            card2 = new QuestionCard("Vad heter Finlands huvudtsad?", "helsingfors");
+            frågekort.Add(card2);
+
+            QuestionCard card3;
+            card3 = new QuestionCard("Vad heter Sveriges huvudstad?", "stockholm");
+            frågekort.Add(card3);
+
+            QuestionCard card4;
+            card4 = new QuestionCard("Vad heter Danmarks huvudstad?", "köpenhamn");
+            frågekort.Add(card4);
+
+            foreach (QuestionCard kort in frågekort)
+            {
+                Console.WriteLine(kort.GetQuestion());
+                string gissning = Console.ReadLine().ToLower();
+                if (kort.IsCorrect(gissning))
+                {
+                    Console.WriteLine("Rätt!");
+                }
+                else
+                {
+                    Console.WriteLine("Fel");
+                }
+            }
+
+
+
+
+
+
+        }
+
+
+
+
 
 
 
@@ -432,6 +614,56 @@ namespace Introduktion_C_
 
     }
 }
+
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      
+
+
+
+        
+        
+        
+         
+        
+
+
+
+
+
+
+
+    
+
+
+
+    
+
+
 
 
 
---
+++ QuestionCard.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Introduktion_C_
+{
+    public class QuestionCard
+    {
+        private string fråga;
+        private string svar;
+
+        public QuestionCard(string f, string s)
+        {
+            fråga = f;
+            svar = s;
+        }
+        public string GetQuestion()
+        {
+            return fråga;
+        }
+
+        public bool IsCorrect(string gissning)
+        {
+            return svar == gissning;
+        }
+    }
+}
